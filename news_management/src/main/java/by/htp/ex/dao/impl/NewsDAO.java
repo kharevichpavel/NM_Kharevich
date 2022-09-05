de380623856159759f87896e40aeffd22719b05f
Pavel_Kharevich <pashacrut84@gmail.com>
2022-09-05 19:15:42 +0300

    Last_05.09.2022

--- news_management/src/main/java/by/htp/ex/dao/impl/NewsDAO.java
+++ news_management/src/main/java/by/htp/ex/dao/impl/NewsDAO.java
@@ -13,9 +13,6 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import by.htp.ex.bean.News;
 import by.htp.ex.dao.INewsDAO;
 import by.htp.ex.dao.NewsDAOException;
